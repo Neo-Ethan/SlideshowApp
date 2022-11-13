098129cf6a6c64f62dbd34f465c9987a96ed580f
Hiro Katoh <katoh@fuji-corpo.co.jp>
2022-11-13 11:39:11 +0900

    var flg = false　if設定版

--- SlideshowApp/ViewController.swift
+++ SlideshowApp/ViewController.swift
@@ -8,12 +8,53 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var imageView: UIImageView!
+    var image1: UIImage!
+    var image2: UIImage!
+    var image3: UIImage!
+    
+    var flg = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        image1 = UIImage(named:"image1")
+        image2 = UIImage(named:"image2")
+        image3 = UIImage(named:"image3")
+        
+        let image = imageView.image
+        imageView.image = image2
+        
     }
-
-
+    
+    @IBAction func tapped(_ sender: Any) {
+        if flg {
+            imageView.image = image1
+            flg = false
+        }
+        else{
+            imageView.image = image2
+            flg = true
+        }
+        
+    }
+    
+    @IBAction func go(_ sender: Any) {
+        
+        
+    }
+    @IBAction func `return`(_ sender: Any) {
+        
+        
+        
+        
+    }
+    @IBAction func auto(_ sender: Any) {
+        
+        
+        
+    }
+    
 }
 
